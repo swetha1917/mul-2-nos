f5efbbd5907a185acfc672c0bd6ff1926a25d931
swetha1917 <76477056+swetha1917@users.noreply.github.com>
2021-01-12 18:26:36 +0530

    developed a program to multiply 2 numbers
    displays multiplied value of the numbers

---
+++ task2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	 double x,y;
+	 double mul;
+	 
+	 cout<<"enter the 1st value:";
+	 cin>>x;
+	 cout<<"enter the 2nd value:";
+	 cin>>y;
+	 mul=x*y;
+	 cout<<"the mul value is :"<<mul;
+	 return 0;
+}
+
